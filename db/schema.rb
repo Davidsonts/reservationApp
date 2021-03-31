5bce1eef1a4413f9a0396e83e60a97caa54d10bb
Davidson Santos <davidsonts@outlook.com>
2021-03-31 08:17:45 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_232753) do
+ActiveRecord::Schema.define(version: 2021_03_31_110435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "data"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "admin"
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_03_29_232753) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "users"
 end
